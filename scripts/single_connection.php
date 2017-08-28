98cb21ee0dd66349c53118507ac4b290abff143a
Sagar Vaidya <imsagar@gmail.com>
2017-08-28 18:16:12 +0530

    scripts

--- scripts/single_connection.php
+++ scripts/single_connection.php
@@ -4,8 +4,8 @@ $db_name = $params[1];
 
 set_time_limit(0);
 $config = parse_ini_file('config.ini', true);
-$pdo[$i] = new PDO('mysql:host='.$config['mysql']['db_host'].';port='.$config['mysql']['db_port'].';dbname='.$db_name, $config['mysql']['db_user'], $config['mysql']['db_pass']);
-$stmt = $pdo[$i]->prepare("select count(*) as total_".$i." from employees");
+$pdo = new PDO('mysql:host='.$config['mysql']['db_host'].';port='.$config['mysql']['db_port'].';dbname='.$db_name, $config['mysql']['db_user'], $config['mysql']['db_pass']);
+$stmt = $pdo->prepare("select count(*) as total from employees");
 $stmt->execute();
 $res = $stmt->fetchAll();	
 print_r($res);
