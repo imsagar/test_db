e342a27772b3e7d96e068b0dac527cd963495eb6
Sagar Vaidya <imsagar@gmail.com>
2017-08-28 13:48:14 +0530

    scripts

--- scripts/dump_dbs.php
+++ scripts/dump_dbs.php
@@ -3,5 +3,7 @@ set_time_limit(0);
 $config = parse_ini_file('config.ini', true);
 
 for($i=1; $i <= $config['default']['total_db']; $i++) {
+	echo "Dumping employee_db_".$i.".sql".PHP_EOL;
 	echo shell_exec('mysql -u'.$config['mysql']['db_user'].' -p'.$config['mysql']['db_pass'].' -h'.$config['mysql']['db_host'].' --port '.$config['mysql']['db_port'].' < employee_db_'.$i.'.sql');
+	echo "Complete".PHP_EOL;
 }
