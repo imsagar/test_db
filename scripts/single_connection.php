2b6c3cdf8dfa6229ebf51a37b1ee14d436e60cf7
Sagar Vaidya <imsagar@gmail.com>
2017-08-28 18:17:10 +0530

    scripts

--- scripts/single_connection.php
+++ scripts/single_connection.php
@@ -2,8 +2,8 @@
 $params = $argv;
 $db_name = $params[1];
 
-set_time_limit(0);
 $config = parse_ini_file('config.ini', true);
+echo "Connecting to DB: ".$db_name.PHP_EOL;
 $pdo = new PDO('mysql:host='.$config['mysql']['db_host'].';port='.$config['mysql']['db_port'].';dbname='.$db_name, $config['mysql']['db_user'], $config['mysql']['db_pass']);
 $stmt = $pdo->prepare("select count(*) as total from employees");
 $stmt->execute();
