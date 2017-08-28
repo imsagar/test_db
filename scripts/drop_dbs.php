18959bd4bc9f80779dcc379087d9a84451e8eb70
Sagar Vaidya <imsagar@gmail.com>
2017-08-28 13:30:16 +0530

    scripts

--- scripts/drop_dbs.php
+++ scripts/drop_dbs.php
@@ -4,6 +4,6 @@ $config = parse_ini_file('config.ini', true);
 $cmd = "mysql -u".$config['mysql']['db_user']." -p".$config['mysql']['db_pass']." -h".$config['mysql']['db_host']." --port ".$config['mysql']['db_port']." -e \"SELECT s.schema_name FROM INFORMATION_SCHEMA.schemata AS s LEFT JOIN INFORMATION_SCHEMA.tables AS t ON t.table_schema = s.schema_name WHERE schema_name LIKE 'test_%' ORDER BY schema_name ASC;\" | grep test | awk -F \"test\" '{print \"DROP DATABASE \" $0 \";\"}'";
 
 $sql_cmd = shell_exec($cmd);
-$sql_cmd .= "mysql -u".$config['mysql']['db_user']." -p".$config['mysql']['db_pass']." -h".$config['mysql']['db_host']." --port ".$config['mysql']['db_port']." -e ".str_replace(PHP_EOL, " ", $sql_cmd);
+$sql_cmd .= "mysql -u".$config['mysql']['db_user']." -p".$config['mysql']['db_pass']." -h".$config['mysql']['db_host']." --port ".$config['mysql']['db_port']." -e '".str_replace(PHP_EOL, " ", $sql_cmd)."'";
 
 echo $sql_cmd;
