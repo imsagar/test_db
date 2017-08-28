75638174b0b49167f4721fe93be718b67a4b689a
Sagar Vaidya <imsagar@gmail.com>
2017-08-28 17:34:19 +0530

    scripts

--- scripts/test_connection.php
+++ scripts/test_connection.php
@@ -5,7 +5,7 @@ $config = parse_ini_file('config.ini', true);
 for($i=1; $i <= $config['default']['total_db']; $i++) {
 	$db_name = 'employee_db_'.$i;
 	$pdo[$i] = new PDO('mysql:host='.$config['mysql']['db_host'].';port='.$config['mysql']['db_port'].';dbname='.$db_name, $config['mysql']['db_user'], $config['mysql']['db_pass']);
-	$stmt = $pdo[$i]->prepare("select count(*) as total_".$i." from employee");
+	$stmt = $pdo[$i]->prepare("select count(*) as total_".$i." from employees");
 	$stmt->execute();
 	$res = $stmt->fetchAll();	
 	print_r($res);
