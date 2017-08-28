5c9fa5d8c63ff2290d343fa5b22c40849a6a7c03
Sagar Vaidya <imsagar@gmail.com>
2017-08-28 13:18:52 +0530

    scripts

--- scripts/dump_dbs.php
+++ scripts/dump_dbs.php
@@ -1,6 +1,6 @@
 <?php
-$db_details = parse_ini_file('database.ini', true);
+$config = parse_ini_file('config.ini', true);
 
-for($i=1; $i <= 3; $i++) {
-	echo shell_exec('mysql -u'.$db_details['mysql']['db_user'].' -p'.$db_details['mysql']['db_pass'].' -h'.$db_details['mysql']['db_host'].' --port '.$db_details['mysql']['db_port'].' < employee_db_'.$i.'.sql');
+for($i=1; $i <= $config['default']['total_db']; $i++) {
+	echo shell_exec('mysql -u'.$config['mysql']['db_user'].' -p'.$config['mysql']['db_pass'].' -h'.$config['mysql']['db_host'].' --port '.$config['mysql']['db_port'].' < employee_db_'.$i.'.sql');
 }
--- scripts/generate_dbs.php
+++ scripts/generate_dbs.php
@@ -1,9 +1,11 @@
 <?php
+$config = parse_ini_file('config.ini', true);
+
 $file = 'employee_schema.txt';
 $rh = fopen($file, 'r');
 $contents = fread($rh, filesize($file));
 fclose($rh);
-for($i=1; $i <= 3; $i++) {
+for($i=1; $i <= $config['default']['total_db']; $i++) {
 	$db_name = 'employee_db_'.$i;
 	$wh = fopen($db_name.'.sql', 'w');
 	fwrite($wh, 'DROP DATABASE IF EXISTS '.$db_name.';'.PHP_EOL);
