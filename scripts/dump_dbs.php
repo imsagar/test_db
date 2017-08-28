2521a32a557225f191481ec74c5d1d10f5acec51
Sagar Vaidya <imsagar@gmail.com>
2017-08-28 12:55:46 +0530

    scripts

--- scripts/dump_dbs.php
+++ scripts/dump_dbs.php
@@ -1,5 +1,5 @@
 <?php
-$db_details = parse_ini_file('database.ini');
+$db_details = parse_ini_file('database.ini', true);
 
 for($i=1; $i <= 3; $i++) {
 	echo shell_exec('mysql -u'.$db_details['mysql']['db_user'].' -p'.$db_details['mysql']['db_pass'].' -h'.$db_details['mysql']['db_host'].' --port '.$db_details['mysql']['db_pass'].' < employees_'.$i.'.sql');
