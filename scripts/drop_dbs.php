29b4bdd69962a186adf54455fb75afe006476f03
Sagar Vaidya <imsagar@gmail.com>
2017-08-28 13:36:17 +0530

    scripts

--- scripts/drop_dbs.php
+++ scripts/drop_dbs.php
@@ -1,7 +1,7 @@
 <?php
 $config = parse_ini_file('config.ini', true);
 
-$cmd = "mysql -u".$config['mysql']['db_user']." -p".$config['mysql']['db_pass']." -h".$config['mysql']['db_host']." --port ".$config['mysql']['db_port']." -e \"SELECT s.schema_name FROM INFORMATION_SCHEMA.schemata AS s LEFT JOIN INFORMATION_SCHEMA.tables AS t ON t.table_schema = s.schema_name WHERE schema_name LIKE 'test_%' ORDER BY schema_name ASC;\" | grep test | awk -F \"test\" '{print \"DROP DATABASE \" $0 \";\"}'";
+$cmd = "mysql -u".$config['mysql']['db_user']." -p".$config['mysql']['db_pass']." -h".$config['mysql']['db_host']." --port ".$config['mysql']['db_port']." -e \"SELECT s.schema_name FROM INFORMATION_SCHEMA.schemata AS s LEFT JOIN INFORMATION_SCHEMA.tables AS t ON t.table_schema = s.schema_name WHERE schema_name LIKE 'employee_db_%' ORDER BY schema_name ASC;\" | grep employee_db_ | awk -F \"employee_db_\" '{print \"DROP DATABASE \" $0 \";\"}'";
 
 $sql_cmd = shell_exec($cmd);
 $sql_cmd .= "mysql -u".$config['mysql']['db_user']." -p".$config['mysql']['db_pass']." -h".$config['mysql']['db_host']." --port ".$config['mysql']['db_port']." -e '".str_replace(PHP_EOL, " ", $sql_cmd)."'";
