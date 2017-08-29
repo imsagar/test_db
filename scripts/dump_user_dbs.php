bef9cd3fd4550f430e5a237e98725663a8129faf
Sagar Vaidya <imsagar@gmail.com>
2017-08-29 17:49:07 +0530

    update

---
+++ scripts/dump_user_dbs.php
@@ -0,0 +1,11 @@
+<?php
+$param = $argv;
+$db_prefix = $argv[1];
+set_time_limit(0);
+$config = parse_ini_file('config.ini', true);
+
+for($i=1; $i <= $config['default']['total_db']; $i++) {
+	echo "Dumping ".$db_prefix.$i.".sql".PHP_EOL;
+	echo shell_exec('mysql -u'.$config['mysql']['db_user'].' -p'.$config['mysql']['db_pass'].' -h'.$config['mysql']['db_host'].' --port '.$config['mysql']['db_port'].' < '.$db_prefix.$i.'.sql');
+	echo "Complete".PHP_EOL;
+}
--- scripts/gen_user_db.php
+++ scripts/gen_user_db.php
@@ -5,7 +5,7 @@ $template = $argv[1];
 $config = parse_ini_file('config.ini', true);
 
 for($i=1; $i <= $config['default']['total_db']; $i++) {
-	$db_name = 'test_mydb_'.$i;
+	$db_name = 'unicourt_'.$i;
 	$wh = fopen($db_name.'.sql', 'w');
 	fwrite($wh, 'DROP DATABASE IF EXISTS '.$db_name.';'.PHP_EOL);
 	fwrite($wh, 'CREATE DATABASE IF NOT EXISTS '.$db_name.';'.PHP_EOL);
