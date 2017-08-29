2876672398ea0bc95479f83deec3f11a000e939a
Sagar Vaidya <imsagar@gmail.com>
2017-08-29 18:27:56 +0530

    update

--- scripts/gen_user_db.php
+++ scripts/gen_user_db.php
@@ -2,10 +2,12 @@
 $params = $argv;
 $template = $argv[1];
 $db_prefix = $argv[2];
+$start = $argv[3]??1;
+$end = $argv[4]??10;
 
 $config = parse_ini_file('config.ini', true);
 
-for($i=1; $i <= $config['default']['total_db']; $i++) {
+for($i=$start; $i <= $end; $i++) {
 	$db_name = $db_prefix.$i;
 	$wh = fopen($db_name.'.sql', 'w');
 	fwrite($wh, 'DROP DATABASE IF EXISTS '.$db_name.';'.PHP_EOL);
