b2cfcf331df949c1b967c13dda813e8e4a10f868
Sagar Vaidya <imsagar@gmail.com>
2017-08-29 17:50:59 +0530

    update

--- scripts/gen_user_db.php
+++ scripts/gen_user_db.php
@@ -1,11 +1,12 @@
 <?php
 $params = $argv;
 $template = $argv[1];
+$db_prefix = $argv[2];
 
 $config = parse_ini_file('config.ini', true);
 
 for($i=1; $i <= $config['default']['total_db']; $i++) {
-	$db_name = 'unicourt_'.$i;
+	$db_name = $db_prefix.$i;
 	$wh = fopen($db_name.'.sql', 'w');
 	fwrite($wh, 'DROP DATABASE IF EXISTS '.$db_name.';'.PHP_EOL);
 	fwrite($wh, 'CREATE DATABASE IF NOT EXISTS '.$db_name.';'.PHP_EOL);
